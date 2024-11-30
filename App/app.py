f8e9a9553f48cbe0038668f55cfa2372b4f0814f
Samuel Harris <harrissamuel130@gmail.com>
2024-11-29 19:44:27 -0500

    presentation-update-1

--- App/app.py
+++ App/app.py
@@ -335,14 +335,9 @@ def main():
         st.write("The interactive tool below allows for comparison of buildings individually from any university. \
                  The tool displays three time series for each variable and plots line plots for each building selected. \
                 ")
-        def run_dash():
-            subprocess.Popen(["/home/adminuser/venv/bin/python3", "App/dash_app.py"])
-        # Start Dash app in a separate thread
-        thread = threading.Thread(target=run_dash)
-        thread.start()
-
-        # Wait for the Dash app to start
-        time.sleep(10)
+        
+        subprocess.Popen(["/home/adminuser/venv/bin/python3", "App/dash_app.py"])
+        
 
         # Embed the Dash app using an iframe
         st.write("Below is the interactive map created using Dash:")
